03977a49711a3f4e98eac70493f3b792e96a76d8
hruodwolf <15839217+hruodwolf@users.noreply.github.com>
2026-01-18 14:43:26 +0100

    add filter for year

--- frontend/src/app/features/records/pages/records/records.ts
+++ frontend/src/app/features/records/pages/records/records.ts
@@ -1,14 +1,14 @@
-import {Component, computed, effect, OnInit, signal, Signal} from '@angular/core';
+import {Component, computed, OnInit, signal, Signal} from '@angular/core';
 import {RecordService} from '../../services/record.service';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {MatCard, MatCardTitle} from '@angular/material/card';
 import {
-  MatCell,
-  MatCellDef,
+  MatCell, MatCellDef,
   MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow,
-  MatRowDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow, MatRowDef,
   MatTable
 } from '@angular/material/table';
 import {DatePipe} from '@angular/common';
@@ -30,15 +30,15 @@ import {RecordCategoryService} from '../../../record-categories/services/record-
     MatHeaderCell,
     MatCell,
     DatePipe,
-    MatHeaderCellDef,
-    MatCellDef,
-    MatRowDef,
     MatRow,
     MatHeaderRow,
     MatButton,
-    MatHeaderRowDef,
     MatFormFieldModule,
     MatSelectModule,
+    MatHeaderRowDef,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatRowDef,
   ],
   templateUrl: './records.html',
   styleUrl: './records.css'
@@ -49,26 +49,31 @@ export class Records implements OnInit  {
 
   readonly categories = signal<RecordCategory[]>([]);
   readonly selectedCategoryId = signal<number | null>(null);
-
+  readonly selectedYear = signal<number | null>(null);
 
 
   displayedColumns: string[] = ['recordCategoryName', 'recordDate', 'recordValue', 'unitName'];
 
-  // Gefilterte Records
   readonly filteredRecords = computed(() => {
-    console.log('filterRec');
-    const selected = this.selectedCategoryId();
+    const selectedCat = this.selectedCategoryId();
+    const selectedYear = this.selectedYear();
     const records = this.recordService.records();
-    console.log(this.years());
-    return selected ? records.filter(r => r.recordCategoryId === selected) : records;
+
+    let tempRecords = selectedCat ? records
+          .filter(r => r.recordCategoryId === selectedCat) : records;
+
+    tempRecords = selectedYear ? tempRecords
+      .filter(r => new Date(r.recordDate).getFullYear() === selectedYear) : tempRecords;
+
+    return tempRecords;
   });
 
-  // wird nur dann ausgeführt wenn oben console.log (this.year)
   readonly years = computed(() => {
-    console.log('in years computed');
-    const years = [...new Set(this.recordService.records().map(r => r.recordDate.substring(0, 4)))];
-    console.log('Years computed:', years);
-    return years;
+    return [...new Set(
+      this.recordService.records()
+        .map(r => new Date(r.recordDate).getFullYear())
+        .filter(y => !isNaN(y))
+    )].sort((a, b) => b - a);
   });
 
   constructor(private recordService: RecordService, private categoryService: RecordCategoryService, private router: Router) { }
@@ -81,7 +86,7 @@ export class Records implements OnInit  {
   }
 
   onCreate() {
-    this.router.navigate(['/records/add']);
+    void this.router.navigate(['/records/add']);
   }
 
 }
