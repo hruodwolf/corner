7232f8074b850c0afeac4f87a971af252b5b61cc
hruodwolf <15839217+hruodwolf@users.noreply.github.com>
2026-07-31 16:53:57 +0200

    add example

--- frontend/src/app/features/measurement-units/pages/measurement-units-subscribe/measurement-units-subscribe.ts
+++ frontend/src/app/features/measurement-units/pages/measurement-units-subscribe/measurement-units-subscribe.ts
@@ -1,7 +1,8 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {MeasurementUnitService} from '../../services/measurement-unit.service';
 import {MeasurementUnit} from '../../../../core/models/measurement-unit.model';
 import {NgFor, NgIf} from '@angular/common';
+import {delay, interval, Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-measurement-units-subscribe',
@@ -13,17 +14,21 @@ import {NgFor, NgIf} from '@angular/common';
   styleUrl: './measurement-units-subscribe.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MeasurementUnitsSubscribe implements OnInit{
+export class MeasurementUnitsSubscribe implements OnInit, OnDestroy{
 
   measurementUnits!: MeasurementUnit[];
 
+  private destroyInterval$ = new Subject<void>();
+
   showNumber!: number;
+
+  countInt: number = 0;
 
   constructor(private measurementUnitService: MeasurementUnitService,
               private cd: ChangeDetectorRef) { }
 
   ngOnInit(){
-    this.measurementUnitService.getAll().subscribe({
+    this.measurementUnitService.getAll().pipe(delay(1000)).subscribe({
       next: (data) => {
         console.log('next' + JSON.stringify(data));
         this.measurementUnits = data;
@@ -35,7 +40,15 @@ export class MeasurementUnitsSubscribe implements OnInit{
       error: (err) => {
         console.log('error' + err);
       }
-    })
+    });
+
+    interval(2000).pipe(takeUntil(this.destroyInterval$)).subscribe({
+      next: (value) => {
+        this.countInt = value;
+        this.cd.markForCheck();
+        console.log(this.countInt);
+        },
+      });
   }
 
   countNumbers(): Promise<number> {
@@ -48,5 +61,18 @@ export class MeasurementUnitsSubscribe implements OnInit{
   async pressButton() {
     console.log("Pressed button");
     this.showNumber = await this.countNumbers();
+  }
+
+  private cleanUpInterval() {
+    this.destroyInterval$.next();
+    this.destroyInterval$.complete();
+  }
+
+  destroyInterval() {
+    this.cleanUpInterval();
+  }
+
+  ngOnDestroy(): void {
+    this.cleanUpInterval();
   }
 }
